28daf9457a039e764a67faab2a1e9764c0377dc1
DH428 <DragonHunter428@gmail.com>
2023-07-21 14:57:07 +0200

    fix: logger not definde

--- modules/returnLatestComVid.js
+++ modules/returnLatestComVid.js
@@ -22,23 +22,26 @@ module.exports = (html) => {
     }
 
     if(member_tab_index < 0){
-        logger(`cant find member tab for '${(this.channel_name ? this.channel_name : this.url_channel)}'`, (this.channel_name ? this.channel_name : ""), "WARNING", this);
-    }else{
-        for (let tab in stuff["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][member_tab_index]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]["itemSectionRenderer"]["contents"]){
-            //simple text posts dont contain ["backstageAttachment"] in json/dict/key value pair thingy or whatever its called
-            try{
-                //unarchived/private(/img) returs undefined
-                if(!stuff["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][member_tab_index]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]["itemSectionRenderer"]["contents"][tab]["videoRenderer"]["videoId"]){
-                    continue;
-                }
-
-                return {
-                    "error": false,
-                    "data": "https://youtube.com/watch?v=" + stuff["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][member_tab_index]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]["itemSectionRenderer"]["contents"][tab]["videoRenderer"]["videoId"],
-                };
-
-            }catch{}
+        return {
+            "error": "cant find member tab",
+            "data": false
         }
+    }
+    
+    for (let tab in stuff["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][member_tab_index]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]["itemSectionRenderer"]["contents"]){
+        //simple text posts dont contain ["backstageAttachment"] in json/dict/key value pair thingy or whatever its called
+        try{
+            //unarchived/private(/img) returs undefined
+            if(!stuff["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][member_tab_index]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]["itemSectionRenderer"]["contents"][tab]["videoRenderer"]["videoId"]){
+                continue;
+            }
+
+            return {
+                "error": false,
+                "data": "https://youtube.com/watch?v=" + stuff["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][member_tab_index]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][1]["itemSectionRenderer"]["contents"][tab]["videoRenderer"]["videoId"],
+            };
+
+        }catch{}
     }
 
     return {
